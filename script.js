20f52ee76e75a387b4857d413aabb113af3e87e3
user <c2h8e9d4@gmail.com>
2020-01-27 23:43:47 +0300

    Усложненное задание 3 урока

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-let number = 266219;
+// var lang = prompt('Выберите Язык'); /
 
-let array = number.toString()
-  .split('')
-  .map(function (item, index) {
-    return parseInt(item);
-  });
+// if (lang === 'ru') {
+//   var weekArr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']; // тут походу я понял в чем отличие var от let =)
+// } else if (lang === 'en') {
+//   var weekArr = ['md', 'th', 'wd', 'td', 'fr', 'st', 'sd']
+// }
+// alert(weekArr);
 
-console.log(array);
-
-let newArr = array.reduce((a, b) => a*b);
-
-console.log(newArr);
-
-let powArr = newArr ** 3;
-
-console.log(powArr);
-
-alert(powArr.toString().substr(0, 2)); // преобразование в строку
+const lang = prompt('Выберите Язык');
 
 
+//-------- Через if ---------//
+// let weekArr;
+// if (lang === 'ru') {
+//   weekArr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// } else if (lang === 'en') {
+//   weekArr = ['md', 'th', 'wd', 'td', 'fr', 'st', 'sd'];
+// }
+// alert(weekArr);
 
+//-------Через switch ------//
+// let weekArr;
+// switch (lang) {
+//   case 'ru':
+//     weekArr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+//     break;
+//   case 'en':
+//     weekArr = ['md', 'th', 'wd', 'td', 'fr', 'st', 'sd'];
+//     break;
+// }
 
+// alert(weekArr);
+//---------Многомерный массив------//
+let weekArrRu = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+let weekArrEn = ['md', 'th', 'wd', 'td', 'fr', 'st', 'sd'];
+let langAnswer = lang === 'ru' ? 0 : 1;
+let arr = [
+  ['ru', weekArrRu],
+  ['eng', weekArrEn]
+];
+
+alert(arr[langAnswer][1]);
+
+//----2.2-------//
+let namePerson = prompt('Укажите имя:');
+let PersonResult = namePerson === 'Артем' ? console.log('Директор') :
+                  namePerson === 'Максим' ? console.log('преподаватель') :
+                  console.log('Студент');
+                    
