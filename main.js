1e6ac4c8adbbca21f4efc5c486f51af320f0d5ef
user <c2h8e9d4@gmail.com>
2020-02-04 23:14:32 +0300

    дз 9 hard

--- main.js
+++ main.js
@@ -1,19 +1,68 @@
-'use strict';
 
-let week = ['ВС','ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-
-let toDay = new Date();
-
-for(let i = 0; i < week.length; i++) {
-    if (i === toDay.getDay() && i === 0 || i === toDay.getDay() && i === 6) {
-      document.write('<p><b><i>' + week[i] + '</i></b></p>');
-    }else if (i === 0 || i === 6) {
-      document.write('<p><i>' + week[i] + '</i></p>');
-    }else if (i === toDay.getDay()) {
-      document.write('<p><b>' + week[i] + '</b></p>');
-    }else{
-      document.write('<p>' + week[i] + '</p>');
+function declens(num, words) {
+  let result;
+  let count = num % 100;
+  if (count >= 5 && count <= 20) {
+    result = words[2];
+  } else {
+    count = count % 10;
+    if (count === 1) {
+      result = words[0];
+    } else if (count >= 2 && count <= 4) {
+      result = words[1];
+    } else {
+      result = words[2];
     }
+  }
+  return result;
 }
 
-console.log(toDay.getDay());
+
+let fild = document.getElementById('time');
+
+function cuurentTime() {
+  let date = new Date();
+  let options = {
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    hour12: false,
+
+  };
+  let a = date.toLocaleString('ru', options);
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  return ('Сегодня ' + a + ' ' + year + ' ' + declens(year,
+    ['год', 'года', 'года']) + ' ' + hours + ' ' + declens(hours,
+    ['час', 'часа', 'часов']) + ' ' + minutes + ' ' + declens(minutes,
+    ['минута', 'минуты', 'минут']) + ' ' + seconds + ' ' + declens(seconds,
+    ['секунда', 'секунды', 'секунд']));
+}
+cuurentTime();
+setInterval( function(){
+  fild.innerHTML = cuurentTime()
+}, 1000);
+
+
+let sFild = document.getElementById('time-2');
+
+function shortDate() {
+  let date = new Date();
+
+  let sHours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  let sMinutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+  let sSeconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  let sDay = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  let sMonth = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+    return (sDay + '.' + sMonth + '.' + date.getFullYear() + '-' + sHours + ':' + sMinutes + ':' + sSeconds);
+}
+shortDate();
+setInterval(function() {
+  sFild.innerHTML = shortDate();
+}, 1000);
+
+
+
+
